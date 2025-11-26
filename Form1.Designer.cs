f3dc72cd3949a4f02c76ee5dd277d0bd1c224f17
vahidcanculfa <vahidcanculfa@outlook.com>
2025-11-26 15:34:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace VCCRfid2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.dgvTags = new System.Windows.Forms.DataGridView();
+            this.ColumnReaderName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEPC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnUserData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAnt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnRSSI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnReadTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnReserved = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotalTags = new System.Windows.Forms.Label();
+            this.txtPromptMessage = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelPort = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelBaud = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblBaudRate = new System.Windows.Forms.Label();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.panelTopControls = new System.Windows.Forms.Panel();
+            this.tmrScan = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTags)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panelTopControls.SuspendLayout();
+            this.SuspendLayout();
+            
+
+            this.btnConnect.Location = new System.Drawing.Point(400, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(80, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            
+
+
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(486, 10);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(80, 23);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            
+
+
+            this.btnScan.Location = new System.Drawing.Point(572, 10);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(80, 23);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            
+
+
+            this.btnStop.Location = new System.Drawing.Point(658, 10);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(80, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            
+
+
+            this.btnClearData.Location = new System.Drawing.Point(744, 10);
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.Size = new System.Drawing.Size(80, 23);
+            this.btnClearData.TabIndex = 4;
+            this.btnClearData.Text = "Clear Data";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.btnClearData_Click);
+            
+
+
+            this.dgvTags.AllowUserToAddRows = false;
+            this.dgvTags.AllowUserToDeleteRows = false;
+            this.dgvTags.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvTags.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTags.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnReaderName,
+            this.ColumnEPC,
+            this.ColumnTID,
+            this.ColumnUserData,
+            this.ColumnAnt,
+            this.ColumnRSSI,
+            this.ColumnReadTime,
+            this.ColumnReserved});
+            this.dgvTags.Location = new System.Drawing.Point(12, 53);
+            this.dgvTags.Name = "dgvTags";
+            this.dgvTags.ReadOnly = true;
+            this.dgvTags.RowHeadersWidth = 51;
+            this.dgvTags.RowTemplate.Height = 24;
+            this.dgvTags.Size = new System.Drawing.Size(833, 300);
+            this.dgvTags.TabIndex = 5;
+            
+
+
+
+            this.ColumnReaderName.HeaderText = "Reader Name";
+            this.ColumnReaderName.MinimumWidth = 6;
+            this.ColumnReaderName.Name = "ColumnReaderName";
+            this.ColumnReaderName.ReadOnly = true;
+            this.ColumnReaderName.Width = 125;
+            
+
+
+
+            this.ColumnEPC.HeaderText = "EPC";
+            this.ColumnEPC.MinimumWidth = 6;
+            this.ColumnEPC.Name = "ColumnEPC";
+            this.ColumnEPC.ReadOnly = true;
+            this.ColumnEPC.Width = 125;
+            
+
+
+
+            this.ColumnTID.HeaderText = "TID";
+            this.ColumnTID.MinimumWidth = 6;
+            this.ColumnTID.Name = "ColumnTID";
+            this.ColumnTID.ReadOnly = true;
+            this.ColumnTID.Width = 125;
+            
+
+
+            this.ColumnUserData.HeaderText = "User Data";
+            this.ColumnUserData.MinimumWidth = 6;
+            this.ColumnUserData.Name = "ColumnUserData";
+            this.ColumnUserData.ReadOnly = true;
+            this.ColumnUserData.Width = 125;
+           
+
+
+            this.ColumnAnt.HeaderText = "Antenna";
+            this.ColumnAnt.MinimumWidth = 6;
+            this.ColumnAnt.Name = "ColumnAnt";
+            this.ColumnAnt.ReadOnly = true;
+            this.ColumnAnt.Width = 75;
+            
+
+
+            this.ColumnRSSI.HeaderText = "RSSI";
+            this.ColumnRSSI.MinimumWidth = 6;
+            this.ColumnRSSI.Name = "ColumnRSSI";
+            this.ColumnRSSI.ReadOnly = true;
+            this.ColumnRSSI.Width = 75;
+            
+
+
+            this.ColumnReadTime.HeaderText = "Read Time";
+            this.ColumnReadTime.MinimumWidth = 6;
+            this.ColumnReadTime.Name = "ColumnReadTime";
+            this.ColumnReadTime.ReadOnly = true;
+            this.ColumnReadTime.Width = 125;
+            
+
+
+            this.ColumnReserved.HeaderText = "Reserved";
+            this.ColumnReserved.MinimumWidth = 6;
+            this.ColumnReserved.Name = "ColumnReserved";
+            this.ColumnReserved.ReadOnly = true;
+            this.ColumnReserved.Width = 125;
+            
+
+
+            this.lblTotalTags.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTotalTags.AutoSize = true;
+            this.lblTotalTags.Location = new System.Drawing.Point(12, 365);
+            this.lblTotalTags.Name = "lblTotalTags";
+            this.lblTotalTags.Size = new System.Drawing.Size(95, 16);
+            this.lblTotalTags.TabIndex = 6;
+            this.lblTotalTags.Text = "Total Number: 0";
+            
+
+
+            this.txtPromptMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPromptMessage.Location = new System.Drawing.Point(12, 390);
+            this.txtPromptMessage.Name = "txtPromptMessage";
+            this.txtPromptMessage.ReadOnly = true;
+            this.txtPromptMessage.Size = new System.Drawing.Size(833, 100);
+            this.txtPromptMessage.TabIndex = 7;
+            this.txtPromptMessage.Text = "";
+            
+
+
+
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelPort,
+            this.toolStripStatusLabelBaud});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 498);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(857, 26);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            
+
+
+            this.toolStripStatusLabelPort.Name = "toolStripStatusLabelPort";
+            this.toolStripStatusLabelPort.Size = new System.Drawing.Size(43, 20);
+            this.toolStripStatusLabelPort.Text = "Port: ";
+            
+
+
+            this.toolStripStatusLabelBaud.Name = "toolStripStatusLabelBaud";
+            this.toolStripStatusLabelBaud.Size = new System.Drawing.Size(51, 20);
+            this.toolStripStatusLabelBaud.Text = "Baud: ";
+           
+
+
+
+            this.cmbPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(60, 10);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 24);
+            this.cmbPorts.TabIndex = 9;
+            
+
+
+
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(10, 13);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(34, 16);
+            this.lblPort.TabIndex = 10;
+            this.lblPort.Text = "Port:";
+            // 
+            // lblBaudRate
+            // 
+            this.lblBaudRate.AutoSize = true;
+            this.lblBaudRate.Location = new System.Drawing.Point(198, 13);
+            this.lblBaudRate.Name = "lblBaudRate";
+            this.lblBaudRate.Size = new System.Drawing.Size(73, 16);
+            this.lblBaudRate.TabIndex = 11;
+            this.lblBaudRate.Text = "Baud Rate:";
+            
+
+
+
+            this.cmbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "921600"});
+            this.cmbBaudRate.Location = new System.Drawing.Point(277, 10);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(117, 24);
+            this.cmbBaudRate.TabIndex = 12;
+            
+
+
+
+            this.panelTopControls.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelTopControls.Controls.Add(this.cmbBaudRate);
+            this.panelTopControls.Controls.Add(this.lblBaudRate);
+            this.panelTopControls.Controls.Add(this.lblPort);
+            this.panelTopControls.Controls.Add(this.cmbPorts);
+            this.panelTopControls.Controls.Add(this.btnConnect);
+            this.panelTopControls.Controls.Add(this.btnDisconnect);
+            this.panelTopControls.Controls.Add(this.btnScan);
+            this.panelTopControls.Controls.Add(this.btnStop);
+            this.panelTopControls.Controls.Add(this.btnClearData);
+            this.panelTopControls.Location = new System.Drawing.Point(12, 0);
+            this.panelTopControls.Name = "panelTopControls";
+            this.panelTopControls.Size = new System.Drawing.Size(833, 47);
+            this.panelTopControls.TabIndex = 13;
+            
+
+
+            this.tmrScan.Tick += new System.EventHandler(this.tmrScan_Tick);
+            
+
+
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 524);
+            this.Controls.Add(this.panelTopControls);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtPromptMessage);
+            this.Controls.Add(this.lblTotalTags);
+            this.Controls.Add(this.dgvTags);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.Text = "VCC RFID Reader Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTags)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panelTopControls.ResumeLayout(false);
+            this.panelTopControls.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.DataGridView dgvTags;
+        private System.Windows.Forms.Label lblTotalTags;
+        private System.Windows.Forms.RichTextBox txtPromptMessage;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPort;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelBaud;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblBaudRate;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.Panel panelTopControls;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnReaderName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEPC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnUserData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAnt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnRSSI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnReadTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnReserved;
+        private System.Windows.Forms.Timer tmrScan;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VCCRfid2
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort = new SerialPort();
+        private readonly byte[] _readCommand = new byte[] { 0xAA, 0x01, 0xBB };
+        private HashSet<string> _uniqueEPCs = new HashSet<string>();
+        private List<byte> _receiveBuffer = new List<byte>();
+
+        private const int EPC_LENGTH_BYTES = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cmbPorts.Items.AddRange(ports);
+            if (ports.Length > 0)
+            {
+                cmbPorts.SelectedIndex = 0;
+            }
+
+            if (cmbBaudRate.Items.Count > 0)
+            {
+                int defaultBaudRateIndex = cmbBaudRate.Items.IndexOf("115200");
+                cmbBaudRate.SelectedIndex = (defaultBaudRateIndex >= 0) ? defaultBaudRateIndex : 0;
+            }
+
+            UpdateStatusLabels("Not Connected", "Not Set");
+            btnScan.Enabled = false;
+            btnStop.Enabled = false;
+            btnDisconnect.Enabled = false;
+
+            tmrScan.Interval = 200;
+            tmrScan.Tick += tmrScan_Tick;
+        }
+
+        private void InitializeDataGridView()
+        {
+            dgvTags.Rows.Clear();
+        }
+
+        private void btnConnect_Click(object? sender, EventArgs e)
+        {
+            if (_serialPort.IsOpen)
+            {
+                UpdatePromptMessage("Already connected.", true);
+                return;
+            }
+
+            string? selectedPort = cmbPorts.SelectedItem?.ToString();
+            string? selectedBaudRate = cmbBaudRate.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedPort) || string.IsNullOrEmpty(selectedBaudRate))
+            {
+                UpdatePromptMessage("Please select Port and Baud Rate.", true);
+                return;
+            }
+
+            try
+            {
+                _serialPort.PortName = selectedPort;
+                _serialPort.BaudRate = int.Parse(selectedBaudRate);
+                _serialPort.DataBits = 8;
+                _serialPort.Parity = Parity.None;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.ReadTimeout = 1000;
+                _serialPort.WriteTimeout = 1000;
+
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
+
+                _serialPort.Open();
+
+                UpdateStatusLabels(_serialPort.PortName, _serialPort.BaudRate.ToString());
+                UpdatePromptMessage($"Connected to {_serialPort.PortName} at {_serialPort.BaudRate} baud.");
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnScan.Enabled = true;
+                btnStop.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                UpdateStatusLabels("Error", "Error");
+                UpdatePromptMessage($"Connection Error: {ex.Message}", true);
+                _serialPort.DataReceived -= _serialPort_DataReceived;
+            }
+        }
+
+        private void btnDisconnect_Click(object? sender, EventArgs e)
+        {
+            DisconnectSerialPort();
+        }
+
+        private void btnScan_Click(object? sender, EventArgs e)
+        {
+            if (_serialPort.IsOpen)
+            {
+                tmrScan.Start();
+                UpdatePromptMessage("Scanning started.");
+                btnScan.Enabled = false;
+                btnStop.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnConnect.Enabled = false;
+            }
+            else
+            {
+                UpdatePromptMessage("Not connected. Please connect first.", true);
+            }
+        }
+
+        private void btnStop_Click(object? sender, EventArgs e)
+        {
+            tmrScan.Stop();
+            UpdatePromptMessage("Scanning stopped.");
+            btnScan.Enabled = true;
+            btnStop.Enabled = false;
+            btnDisconnect.Enabled = true;
+            btnConnect.Enabled = false;
+        }
+
+        private void btnClearData_Click(object? sender, EventArgs e)
+        {
+            ClearTagData();
+            UpdatePromptMessage("Tag data cleared.");
+        }
+
+        private void tmrScan_Tick(object? sender, EventArgs e)
+        {
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.Write(_readCommand, 0, _readCommand.Length);
+                }
+                else
+                {
+                    tmrScan.Stop();
+                    UpdatePromptMessage("Port disconnected. Stopping scan.", true);
+                    SetControlStates(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                tmrScan.Stop();
+                UpdatePromptMessage($"Scan write error: {ex.Message}", true);
+                SetControlStates(false);
+            }
+        }
+
+        private void _serialPort_DataReceived(object? sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort? sp = sender as SerialPort;
+                if (sp == null) return;
+
+                int bytesToRead = sp.BytesToRead;
+                byte[] data = new byte[bytesToRead];
+                sp.Read(data, 0, bytesToRead);
+
+                lock (_receiveBuffer)
+                {
+                    _receiveBuffer.AddRange(data);
+                }
+
+                ProcessReceivedData();
+            }
+            catch (Exception ex)
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(new Action(() => UpdatePromptMessage($"Data Receive Error: {ex.Message}", true)));
+                }
+                else
+                {
+                    UpdatePromptMessage($"Data Receive Error: {ex.Message}", true);
+                }
+            }
+        }
+
+        private void ProcessReceivedData()
+        {
+            lock (_receiveBuffer)
+            {
+                while (_receiveBuffer.Count >= EPC_LENGTH_BYTES)
+                {
+                    byte[] epcBytes = _receiveBuffer.Take(EPC_LENGTH_BYTES).ToArray();
+                    string epc = BitConverter.ToString(epcBytes).Replace("-", "");
+
+                    _receiveBuffer.RemoveRange(0, EPC_LENGTH_BYTES);
+
+                    AddOrUpdateTag(epc);
+                }
+            }
+        }
+
+        private void AddOrUpdateTag(string epc)
+        {
+            if (dgvTags.InvokeRequired)
+            {
+                dgvTags.Invoke(new Action(() => AddOrUpdateTag(epc)));
+                return;
+            }
+
+            if (_uniqueEPCs.Add(epc))
+            {
+                dgvTags.Rows.Add("Reader1", epc, "", "", "1", "N/A", DateTime.Now.ToString("HH:mm:ss"), "");
+            }
+            else
+            {
+                foreach (DataGridViewRow row in dgvTags.Rows)
+                {
+                    if (row.Cells["ColumnEPC"].Value?.ToString() == epc)
+                    {
+                        row.Cells["ColumnReadTime"].Value = DateTime.Now.ToString("HH:mm:ss");
+                        break;
+                    }
+                }
+            }
+
+            UpdateTotalTags(_uniqueEPCs.Count);
+        }
+
+        private void ClearTagData()
+        {
+            dgvTags.Rows.Clear();
+            _uniqueEPCs.Clear();
+            UpdateTotalTags(0);
+        }
+
+        private void DisconnectSerialPort()
+        {
+            if (_serialPort.IsOpen)
+            {
+                tmrScan.Stop();
+                try
+                {
+                    _serialPort.DataReceived -= _serialPort_DataReceived;
+                    _serialPort.Close();
+                    UpdatePromptMessage($"Disconnected from {_serialPort.PortName}.");
+                }
+                catch (Exception ex)
+                {
+                    UpdatePromptMessage($"Disconnection Error: {ex.Message}", true);
+                }
+            }
+
+            SetControlStates(true);
+            UpdateStatusLabels("Not Connected", "Not Set");
+        }
+
+        private void SetControlStates(bool isDisconnected)
+        {
+            btnConnect.Enabled = isDisconnected;
+            btnDisconnect.Enabled = !isDisconnected;
+            btnScan.Enabled = !isDisconnected && !tmrScan.Enabled;
+            btnStop.Enabled = !isDisconnected && tmrScan.Enabled;
+        }
+
+
+        private void UpdateTotalTags(int count)
+        {
+            if (lblTotalTags.InvokeRequired)
+            {
+                lblTotalTags.Invoke(new Action(() => lblTotalTags.Text = $"Total Number: {count}"));
+            }
+            else
+            {
+                lblTotalTags.Text = $"Total Number: {count}";
+            }
+        }
+
+        private void UpdatePromptMessage(string message, bool isError = false)
+        {
+            if (txtPromptMessage.InvokeRequired)
+            {
+                txtPromptMessage.Invoke(new Action(() => UpdatePromptMessage(message, isError)));
+            }
+            else
+            {
+                if (isError)
+                {
+                    txtPromptMessage.SelectionColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    txtPromptMessage.SelectionColor = System.Drawing.Color.Black;
+                }
+
+                txtPromptMessage.AppendText($"{DateTime.Now.ToString("HH:mm:ss")} - {message}{Environment.NewLine}");
+                txtPromptMessage.SelectionStart = txtPromptMessage.TextLength;
+                txtPromptMessage.ScrollToCaret();
+                txtPromptMessage.SelectionColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void UpdateStatusLabels(string? portStatus, string? baudStatus)
+        {
+            if (statusStrip1.InvokeRequired)
+            {
+                statusStrip1.Invoke(new Action(() => UpdateStatusLabels(portStatus ?? "N/A", baudStatus ?? "N/A")));
+            }
+            else
+            {
+                toolStripStatusLabelPort.Text = $"Port: {portStatus ?? "Not Set"}";
+                toolStripStatusLabelBaud.Text = $"Baud: {baudStatus ?? "Not Set"}";
+            }
+        }
+    }
+}
